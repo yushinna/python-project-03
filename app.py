9780fa75675731ffa91e0607d479948f8970bffd
yushinna <yuminnakao@Yumin-Nakao.local>
2019-09-18 15:06:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
+
+```
+Update the app.py file
+Import your Game class from the phrasehunter package.
+Create a new instance of the Game class and store this instance in a game variable.
+Call the instance method you create for your Game instance that kicks off the game loop, this should start the game in the console.
+Ensure you place your instance creation and method calls inside a Dunder Main statement.
+You must use OOP principles for the entire constructions game meaning no helper functions should be used outside of the classes built above. The entire game should run solely on creating an instance of a Game class and method calls within that instance.
+```
