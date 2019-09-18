2a1e13ee2bf9e67ed4daab67c5dc7d4524d72d2f
yushinna <yuminnakao@Yumin-Nakao.local>
2019-09-18 15:06:43 +0900

    Update phrase.py

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,9 @@
 # Create your Phrase class logic here.
+
+```
+Create the Phrase class in the phrase.py file
+The class should include an initializer or def __init__ that receives a phrase parameter and holds this phrase in an instance attribute on the Phrase object. A phrase should be a collection of Character objects, where each letter of the phrase is a Character() instance stored inside a collection such as a List.
+Any additional instance attributes you feel you need to store are up to you.
+The Phrase instance might be responsible for things like: Knowing if the entire phrase has been guessed, Iterating over its collection of Character to allow a guess to be checked using a Character instance method call or to ask the Character object how it should show its letter.
+The instance method names and their implementation are up to you to determine.
+```
