94096541d052f77d451821f627fdcb80b16d75f4
yushinna <yuminnakao@Yumin-Nakao.local>
2019-09-18 15:06:41 +0900

    Update game.py

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,9 @@
 # Create your Game class logic in here.
+
+```
+Create the Game class in the game.py file
+The class should include an initializer or def __init__ method that receives a phrases parameter and holds these phrases in an instance attribute on the game object.
+You will likely need at least 1 instance attribute:
+An instance attribute that stores the current Phrase object for the start of the game. You may think of this as the Active phrase being guessed against by the player while the game is running.
+The Game instance might be responsible for things like: starting the game loop, getting player's input() guesses to pass to a Phrase object to perform its responsibilities against, determining if a win/loss happens after the player runs out of turns or the phrase is completely guessed.
+```
