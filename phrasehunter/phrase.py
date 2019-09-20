bf6e641fdb261515c6e03b9f1d49642a96a146b1
yushinna <yuminnakao@Yumin-Nakao.local>
2019-09-20 21:52:04 +0900

    Update phrase.py

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,21 @@
-# Create your Phrase class logic here.
+class Phrase:
+    """
+    Phrase instance is to handle the creation of phrases
+    """
+    def __init__(self, phrase: list):
+        """
+        phrase: list, a collection of Character objects
+        """
+        self.phrase = phrase
+
+    def original(self):
+        return list(map(lambda x: x.original, self.phrase))
+
+    def all_guessed(self) -> bool:
+        if all(list(map(lambda x: x.was_guessed, self.phrase))):
+            return True
+        else:
+            return False
+
+    def show(self) -> list:
+        return list(map(lambda x: x.show(), self.phrase))
